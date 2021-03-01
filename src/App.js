3c12b5acd23f8aea7bdc20efcde48b58f71ca109
gautamrwx <gautam.404@protonmail.com>
2021-03-01 16:23:02 +0530

    Added functionalities to tictactoe board

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Board from './components/Board';
 import './styles/root.scss';
+
 const App = () => {
   return (
     <div className="app">
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const Square = ({ value }) => {
+const Square = ({ value, clickAction }) => {
   return (
-    <button className="square" type="button">
+    <button className="square" type="button" onClick={clickAction}>
       {value}
     </button>
   );
